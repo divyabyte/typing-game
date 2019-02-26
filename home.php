6cb553e4273ff4d0835c35a44c3e730edd8380c1
divyabyte <divyasinghal041@gmail.com>
2019-02-26 18:10:58 +0530

    working on layout

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+	<title></title>
+</head>
+<body>
+<section  id="projects">
+	<div class="container">
+		<center>
+			<h3 class="tittle-w3 text-uppercase mb-lg-5 mb-3 text-center">Categories</h3>
+		</center>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="project-card"">
+					<a href="key.html">
+					<img src="images/key.jpg" height="300" width="500" class="img-fluid" /></a>
+					<pd>Key Typing</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="project-card"">
+					<a href="speed.html">
+					<img src="images/speed.jpg" height="300" width="500" class="img-fluid" /></a>
+					<p>Speed Test</p>
+				</div>
+			</div>
+		</div>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="project-card"">
+					<a href="word.html">
+					<img src="images/word.jpg" height="300" width="500" class="img-fluid" /></a>
+					<p>Word Typing</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="project-card"">
+					<a href="sen.html">
+					<img src="images/sen.jpg" height="300" width="500" class="img-fluid" /></a>
+					<p>Sentence Typing</p>
+				</div>
+		   </div>
+	   </div>
+	</div>
+</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body class="bgimage">
+
+<center>
+<a href="home.php"><button class="play">Play</button></a>
+</center>
+</body>
+</html>
